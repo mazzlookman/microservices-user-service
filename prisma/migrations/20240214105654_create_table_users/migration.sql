b3ea3bcc1737fb37f51e74e27cf2a4b8d2e9833b
mazzlookman <mohammadlukmanaqib@gmail.com>
2024-02-14 18:27:51 +0700

    prisma model, migration, seeder for user

---
+++ prisma/migrations/20240214105654_create_table_users/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `profession` VARCHAR(255) NOT NULL,
+    `avatar` VARCHAR(255) NULL,
+    `role` ENUM('student', 'admin') NOT NULL DEFAULT 'student',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/seeder/user-seeder.js
@@ -0,0 +1,36 @@
+import {prismaClient} from "../../src/app/database/prisma-client.js";
+import bcrypt from "bcrypt";
+
+const createManyUsers = async () => {
+    const manyUsers = await prismaClient.user.createMany({
+        data: [
+            {
+                name: "Aqib",
+                email: "aqib@test.com",
+                password: await bcrypt.hash("123", 10),
+                profession: "Backend",
+                avatar: "aqib.png",
+                role: "admin",
+            },
+            {
+                name: "Ucup",
+                email: "ucup@test.com",
+                password: await bcrypt.hash("123", 10),
+                profession: "Frontend",
+                avatar: "ucup.png",
+            }
+        ]
+    });
+
+    console.log(`Success insert ${manyUsers.count} users`);
+}
+
+createManyUsers()
+    .then(async () => {
+        await prismaClient.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prismaClient.$disconnect()
+        process.exit(1)
+    });
